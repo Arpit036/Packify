9aee27bc3d8c48a69fc99c96941a8403620ae34e
Arpit  Kumar Verma <132037701+Arpit036@users.noreply.github.com>
2025-11-09 13:04:04 +0530

    code till basics of routes

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const app = express();
 
 const cookieParser = require("cookie-parser")
 const path  = require("path")
+const db = require('./config/mongooseConnection')
+const ownersRouter = require("./routes/ownersRouter")
+const productsRouter = require("./routes/productsRouter")
+const usersRouter = require("./routes/usersRouter")
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
@@ -13,5 +17,9 @@ app.set("view engine", "ejs")
 app.get("/", (req, res)=>{
     res.send("hey");
 })
+
+app.use("/owners", ownersRouter);
+app.use("/users", usersRouter);
+app.use("/products", productsRouter)
 
 app.listen(3000)
